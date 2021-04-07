2c24c3e1d94e3ff9b1fc21b47f8b0a9c08929254
Antoine Beauchamp <end2endzone@gmail.com>
2021-04-07 19:11:06 -0400

    Updated travis install_win32arduino.sh script as per github script

--- ci/travis/install_win32arduino.sh
+++ ci/travis/install_win32arduino.sh
@@ -3,39 +3,42 @@ set -e
 
 # Validate Travis CI environment
 if [ "$TRAVIS_BUILD_DIR" = "" ]; then
-  echo "Please define 'TRAVIS_BUILD_DIR' environment variable.";
+  echo "Please define 'GITHUB_WORKSPACE' environment variable.";
   exit 1;
 fi
 
-export GTEST_ROOT=$TRAVIS_BUILD_DIR/third_parties/googletest/install
-export rapidassist_DIR=$TRAVIS_BUILD_DIR/third_parties/RapidAssist/install
-export win32arduino_DIR=$TRAVIS_BUILD_DIR/third_parties/win32Arduino/install
-echo win32arduino_DIR=$win32arduino_DIR
+export CMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR/third_parties/win32Arduino/install
+unset CMAKE_PREFIX_PATH
+export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH;$TRAVIS_BUILD_DIR/third_parties/googletest/install"
+export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH;$TRAVIS_BUILD_DIR/third_parties/RapidAssist/install"
+export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH;$TRAVIS_BUILD_DIR/third_parties/win32Arduino/install"
 
 echo ============================================================================
 echo Cloning win32Arduino into $TRAVIS_BUILD_DIR/third_parties/win32Arduino
 echo ============================================================================
 mkdir -p $TRAVIS_BUILD_DIR/third_parties
 cd $TRAVIS_BUILD_DIR/third_parties
-git clone "https://github.com/end2endzone/win32Arduino.git"
+#git clone "https://github.com/end2endzone/win32Arduino.git"
 cd win32Arduino
 echo
 
 echo Checking out version 2.4.0...
-git checkout 2.4.0
+git -c advice.detachedHead=false checkout 2.4.0
 echo
 
 echo ============================================================================
-echo Compiling...
+echo Compiling win32Arduino...
 echo ============================================================================
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$win32arduino_DIR ..
+echo Configure...
+cmake -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH" ..
+echo Buliding...
 cmake --build .
 echo
 
 echo ============================================================================
-echo Installing into $win32arduino_DIR
+echo Installing win32Arduino into $CMAKE_INSTALL_PREFIX
 echo ============================================================================
 make install
 echo
